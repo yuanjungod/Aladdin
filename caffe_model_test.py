86ec35b1cc3d8ff93d411f605ba68765f8d272e9
yuanjungod <quantum_matrix@sina.com>
2018-07-05 19:51:17 +0800

    asd

--- caffe_model_test.py
+++ caffe_model_test.py
@@ -5,50 +5,56 @@ from inferrence import *
 
 date_root = "/alidata/home/yuanjun/data/train_data/train"
 face_dir_list = os.listdir(date_root)
-total_count = 1
-right_count = 1
-same_people_right_count = 1
-same_people_count = 1
-diff_people_right_count = 1
-diff_people_count = 1
+
+true_positive = 0
+false_negative = 0
+true_negative = 0
+false_positive = 0
+
+positive = 0
+negative = 0
+
+
 while True:
     left = random.choice(face_dir_list)
     right = random.choice(face_dir_list)
     if random.random() > 0.6:
         right = left
+
+    pic1 = os.path.join(date_root, left, random.choice(os.listdir(os.path.join(date_root, left))))
+    pic2 = os.path.join(date_root, right, random.choice(os.listdir(os.path.join(date_root, right))))
+    distance = get_distance(net, transformer, pic1, pic2)
+
     if left == right:
-        if total_count % 30 == 0:
-            print("same people")
-        pic1 = os.path.join(date_root, left, random.choice(os.listdir(os.path.join(date_root, left))))
-        pic2 = os.path.join(date_root, right, random.choice(os.listdir(os.path.join(date_root, right))))
+        positive += 1
+    else:
+        negative += 1
 
-        distance = get_distance(net, transformer, pic1, pic2)
-        same_people_count += 1
-        if total_count % 30 == 0:
-            print(distance)
-        total_count += 1
-        if distance <= 0.9:
-            right_count += 1
-            same_people_right_count += 1
+    if distance <= 0.9:
+        if left == right:
+            true_positive += 1
+        else:
+            false_positive += 1
+    elif distance > 1:
+        if left == right:
+            false_negative += 1
+        else:
+            true_negative += 1
 
-    elif left != right:
-        if total_count % 30 == 0:
-            print("different people")
-        pic1 = os.path.join(date_root, left, random.choice(os.listdir(os.path.join(date_root, left))))
-        pic2 = os.path.join(date_root, right, random.choice(os.listdir(os.path.join(date_root, right))))
-        distance = get_distance(net, transformer, pic1, pic2)
-        diff_people_count += 1
-        if total_count % 30 == 0:
-            print(distance)
-        total_count += 1
-        if distance > 1.0:
-            right_count += 1
-            diff_people_right_count += 1
-    if total_count % 30 == 0:
-        print("total right rate", total_count, right_count, (right_count*1.0)/total_count)
-        print("same people right rate", same_people_count, same_people_right_count, (same_people_right_count*1.0)/same_people_count)
-        print("diff people right rate", diff_people_count, diff_people_right_count, (diff_people_right_count*1.0)/diff_people_count)
-        print("#####################################################################################")
+    if positive > 0 and negative > 0 and (positive+negative) % 100 == 0:
+
+        print("true positive rate: %s" % ((1.0*true_positive)/(true_positive+false_positive)))
+        print("false positive rate: %s" % ((1.0*false_positive)/(true_positive+false_positive)))
+        print("positive recall: %s" % 1.0*true_positive/positive)
+
+        print("#################################################################################")
+
+        print("true negative rate: %s" % ((1.0*true_negative)/(true_negative+false_negative)))
+        print("false negative rate: %s" % ((1.0*false_negative)/(true_negative+false_negative)))
+        print("negative recall: %s" % 1.0*true_negative/negative)
+
+
+
 
 
 
