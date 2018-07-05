332450b1776de107bf5c09ed5662b8519230e9c7
Happy <happy@HappydeMacBook-Pro.local>
2018-07-05 14:46:39 +0800

    train set

--- image_data_layer_train_data.py
+++ image_data_layer_train_data.py
@@ -4,6 +4,17 @@ import random
 root_folder = "/alidata/home/yuanjun/data/train_data/train/"
 
 
+def random_choice(image_folder_files, count):
+    if len(image_folder_files) < count:
+        print("len(image_folder_files) must > count")
+    choice_list = set()
+    index_list = set(range(len(image_folder_files)))
+    for i in range(count):
+        index_list = index_list - choice_list
+        choice_list.add(random.choice(list(index_list)))
+    return choice_list
+
+
 def train_data():
     train_txt = open("train.txt", "w+")
     image_folders = os.listdir(root_folder)
@@ -14,9 +25,9 @@ def train_data():
         image_folder = random.choice(image_folders)
         image_folder_files = os.listdir(os.path.join(root_folder, image_folder))
         if len(image_folder_files) > 3:
-            begin = random.choice(range(len(image_folder_files)-3))
-            for i in range(3):
-                train_txt.write("%s %s\n" % (os.path.join(image_folder, image_folder_files[begin+i]), image_folder))
+            # begin = random.choice(range(len(image_folder_files)-3))
+            for i in random_choice(image_folder_files, 3):
+                train_txt.write("%s %s\n" % (os.path.join(image_folder, image_folder_files[i]), image_folder))
         else:
             for i in range(len(image_folder_files)):
                 train_txt.write("%s %s\n" % (os.path.join(image_folder, image_folder_files[i]), image_folder))
@@ -36,9 +47,8 @@ def val_data():
         image_folder = random.choice(image_folders)
         image_folder_files = os.listdir(os.path.join(root_folder, image_folder))
         if len(image_folder_files) > 3:
-            begin = random.choice(range(len(image_folder_files)-3))
-            for i in range(3):
-                val_txt.write("%s %s\n" % (os.path.join(image_folder, image_folder_files[begin+i]), image_folder))
+            for i in random_choice(image_folder_files, 3):
+                val_txt.write("%s %s\n" % (os.path.join(image_folder, image_folder_files[i]), image_folder))
         else:
             for i in range(len(image_folder_files)):
                 val_txt.write("%s %s\n" % (os.path.join(image_folder, image_folder_files[i]), image_folder))
